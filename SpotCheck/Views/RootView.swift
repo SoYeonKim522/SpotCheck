c095081ae01ec2ccd5c0e24da5366ae036dd6394
Soyeon Kim <sykim096@gmail.com>
2026-09-07 23:58:09 +1000

    chore: set up project folder structure

---
+++ SpotCheck/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  SpotCheck
+//
+//  Created by MACBOOK_PRO on 7/9/2026.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("SpotCheck")
+    }
+}
+
+#Preview {
+    RootView()
+}
