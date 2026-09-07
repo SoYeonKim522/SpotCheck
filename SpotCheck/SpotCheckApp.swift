d7c530f0aae0dadc6163616eb6f39982544d02ef
Soyeon Kim <sykim096@gmail.com>
2026-09-07 23:12:51 +1000

    chore: Initial Commit

---
+++ SpotCheck/SpotCheckApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpotCheckApp.swift
+//  SpotCheck
+//
+//  Created by MACBOOK_PRO on 7/9/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpotCheckApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SpotCheckTests/SpotCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotCheckTests.swift
+//  SpotCheckTests
+//
+//  Created by MACBOOK_PRO on 7/9/2026.
+//
+
+import Testing
+@testable import SpotCheck
+
+struct SpotCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
